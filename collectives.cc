e3e76594cb9aa7bed49566c896584f5d700d9c78
Frédéric Gessler <frederic.gessler@epfl.ch>
2020-02-29 11:23:43 +0100

    Inline ncclcomminitrank

--- collectives.cc
+++ collectives.cc
@@ -1,9 +1,40 @@
 #include <torch/extension.h>
-#include "src/nccl.h"
+#include "src/include/transport.h"
 
 #define CHECK_CUDA(x) TORCH_CHECK(x.type().is_cuda(), #x " must be a CUDA tensor")
 #define CHECK_CONTIGUOUS(x) TORCH_CHECK(x.is_contiguous(), #x " must be contiguous")
 #define CHECK_INPUT(x) CHECK_CUDA(x); CHECK_CONTIGUOUS(x)
+
+static ncclResult_t ncclCommInitRankDev(ncclComm_t* newcomm, int nranks, ncclUniqueId commId, int myrank, int cudaDev) {
+  ncclResult_t res;
+  char* env = getenv("NCCL_COMM_ID");
+  if (env && myrank == 0) {
+    NCCLCHECKGOTO(bootstrapCreateRoot(&commId, true), res, end);
+  }
+
+  NCCLCHECKGOTO(ncclInit(), res, end);
+  if (myrank == 0) showVersion();
+
+  // Make sure the CUDA runtime is initialized.
+  CUDACHECKGOTO(cudaFree(NULL), res, end);
+
+  NCCLCHECKGOTO(PtrCheck(newcomm, "CommInitRank", "newcomm"), res, end);
+  if (nranks < 1 || myrank < 0 || myrank >= nranks) {
+    WARN("Invalid rank requested : %d/%d", myrank, nranks);
+    res = ncclInvalidArgument;
+    goto end;
+  }
+
+  if (ncclAsyncMode()) {
+    NCCLCHECKGOTO(ncclAsyncInit(ncclCommInitRankSync, newcomm, nranks, commId, myrank, cudaDev), res, end);
+  } else {
+    NCCLCHECKGOTO(ncclCommInitRankSync(newcomm, nranks, commId, myrank, cudaDev), res, end);
+  }
+end:
+  if (ncclAsyncMode()) return ncclAsyncErrCheck(res);
+  else return res;
+}
+
 
 void init() {
     ncclComm_t comms[4];
