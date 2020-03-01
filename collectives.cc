41f1893851a1766d6c2c7e4178e4cea8eb6fda15
Frédéric Gessler <frederic.gessler@epfl.ch>
2020-03-01 21:16:04 +0100

    Add MPI/NCCL example from nvidia to cuda extension

--- collectives.cc
+++ collectives.cc
@@ -1,9 +1,33 @@
 #include <torch/extension.h>
 #include <nccl.h>
+#include <mpi.h>
+#include <cstdlib>
+#include <iostream>
+
+
 
 #define CHECK_CUDA(x) TORCH_CHECK(x.type().is_cuda(), #x " must be a CUDA tensor")
 #define CHECK_CONTIGUOUS(x) TORCH_CHECK(x.is_contiguous(), #x " must be contiguous")
 #define CHECK_INPUT(x) CHECK_CUDA(x); CHECK_CONTIGUOUS(x)
+
+#define MPICHECK(cmd) do {                          \
+  int e = cmd;                                      \
+  if( e != MPI_SUCCESS ) {                          \
+    printf("Failed: MPI error %s:%d '%d'\n",        \
+        __FILE__,__LINE__, e);   \
+    exit(EXIT_FAILURE);                             \
+  }                                                 \
+} while(0)
+
+
+#define CUDACHECK(cmd) do {                         \
+  cudaError_t e = cmd;                              \
+  if( e != cudaSuccess ) {                          \
+    printf("Failed: Cuda error %s:%d '%s'\n",             \
+        __FILE__,__LINE__,cudaGetErrorString(e));   \
+    exit(EXIT_FAILURE);                             \
+  }                                                 \
+} while(0)
 
 #define NCCLCHECK(cmd) do {                         \
   ncclResult_t r = cmd;                             \
@@ -14,7 +38,102 @@
   }                                                 \
 } while(0)
 
+static uint64_t getHostHash(const char* string) {
+  // Based on DJB2, result = result * 33 + char
+  uint64_t result = 5381;
+  for (int c = 0; string[c] != '\0'; c++){
+    result = ((result << 5) + result) + string[c];
+  }
+  return result;
+}
 
+
+static void getHostName(char* hostname, int maxlen) {
+  gethostname(hostname, maxlen);
+  for (int i=0; i< maxlen; i++) {
+    if (hostname[i] == '.') {
+        hostname[i] = '\0';
+        return;
+    }
+  }
+}
+
+void allreduce()
+{
+  int size = 32*1024*1024;
+
+
+  int myRank, nRanks, localRank = 0;
+    int argc = 1;
+     char* argv[3];
+
+  //initializing MPI
+  MPICHECK(MPI_Init(&argc, &argv));
+  MPICHECK(MPI_Comm_rank(MPI_COMM_WORLD, &myRank));
+  MPICHECK(MPI_Comm_size(MPI_COMM_WORLD, &nRanks));
+
+
+  //calculating localRank based on hostname which is used in selecting a GPU
+  uint64_t hostHashs[nRanks];
+  char hostname[1024];
+  getHostName(hostname, 1024);
+  hostHashs[myRank] = getHostHash(hostname);
+  MPICHECK(MPI_Allgather(MPI_IN_PLACE, 0, MPI_DATATYPE_NULL, hostHashs, sizeof(uint64_t), MPI_BYTE, MPI_COMM_WORLD));
+  for (int p=0; p<nRanks; p++) {
+     if (p == myRank) break;
+     if (hostHashs[p] == hostHashs[myRank]) localRank++;
+  }
+
+
+  ncclUniqueId id;
+  ncclComm_t comm;
+  float *sendbuff, *recvbuff;
+  cudaStream_t s;
+
+
+  //get NCCL unique ID at rank 0 and broadcast it to all others
+  if (myRank == 0) ncclGetUniqueId(&id);
+  MPICHECK(MPI_Bcast((void *)&id, sizeof(id), MPI_BYTE, 0, MPI_COMM_WORLD));
+
+
+  //picking a GPU based on localRank, allocate device buffers
+  CUDACHECK(cudaSetDevice(localRank));
+  CUDACHECK(cudaMalloc(&sendbuff, size * sizeof(float)));
+  CUDACHECK(cudaMalloc(&recvbuff, size * sizeof(float)));
+  CUDACHECK(cudaStreamCreate(&s));
+
+
+  //initializing NCCL
+  NCCLCHECK(ncclCommInitRank(&comm, nRanks, id, myRank));
+
+
+  //communicating using NCCL
+  NCCLCHECK(ncclAllReduce((const void*)sendbuff, (void*)recvbuff, size, ncclFloat, ncclSum,
+        comm, s));
+
+
+  //completing NCCL operation by synchronizing on the CUDA stream
+  CUDACHECK(cudaStreamSynchronize(s));
+
+
+  //free device buffers
+  CUDACHECK(cudaFree(sendbuff));
+  CUDACHECK(cudaFree(recvbuff));
+
+
+  //finalizing NCCL
+  ncclCommDestroy(comm);
+
+
+  //finalizing MPI
+  MPICHECK(MPI_Finalize());
+
+
+  printf("[MPI Rank %d] Success \n", myRank);
+}
+
+
+//ncclGetErrorString
 void init(int nDev) {
     ncclComm_t comms[nDev];
     //int size = 32*1024*1024;
@@ -23,9 +142,38 @@ void init(int nDev) {
         devs[i] = i;
     }
     NCCLCHECK(ncclCommInitAll(comms, nDev, devs));
+    int count = 0;
+    NCCLCHECK(ncclCommCount(comms, &count));
+    std::cout << count << std::endl;
     /*ncclCommInitAll(comms, nDev, devs);*/
+}
+
+void allgather(torch::Tensor tensor, int nDev) {
+  //allocating and initializing device buffers
+  float** sendbuff = (float**)malloc(nDev * sizeof(float*));
+  float** recvbuff = (float**)malloc(nDev * sizeof(float*));
+  cudaStream_t* s = (cudaStream_t*)malloc(sizeof(cudaStream_t)*nDev);
+
+
+  for (int i = 0; i < nDev; ++i) {
+    CUDACHECK(cudaSetDevice(i));
+    CUDACHECK(cudaMalloc(sendbuff + i, size * sizeof(float)));
+    CUDACHECK(cudaMalloc(recvbuff + i, size * sizeof(float)));
+    CUDACHECK(cudaMemset(sendbuff[i], 1, size * sizeof(float)));
+    CUDACHECK(cudaMemset(recvbuff[i], 0, size * sizeof(float)));
+    CUDACHECK(cudaStreamCreate(s+i));
+  }
+
+
+  //initializing NCCL
+  NCCLCHECK(ncclCommInitAll(comms, nDev, devs));
+
+
+
 }
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
   m.def("init", &init, "init");
+  m.def("allreduce", &allreduce, "allreduce");
 }
+
