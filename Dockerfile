4510fb6f7e356a5c9500f09bc89dc2946a171ad9
Frédéric Gessler <frederic.gessler@epfl.ch>
2020-03-02 18:07:56 +0100

    Remove mpi from api

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,17 @@
-FROM nvidia/cuda:10.1-base-ubuntu18.04
-RUN apt-get update -y
-RUN apt-get install -y build-essential nvidia-cuda-toolkit git 
-
-ADD . /nccl
+FROM nvidia/cuda:10.1-base-ubuntu18.04 
+ENV LIB /pyparsa
+RUN apt-get update -y 
+RUN apt-get install -y --no-install-recommends python3 python3-virtualenv build-essential python3-dev iproute2 procps git cmake autoconf automake autotools-dev g++ pkg-config libtool git wget nvidia-cuda-toolkit libopenmpi-dev openmpi-bin libhdf5-openmpi-dev
+ENV VIRTUAL_ENV=/opt/venv
+RUN python3 -m virtualenv --python=/usr/bin/python3 $VIRTUAL_ENV
+ENV PATH="$VIRTUAL_ENV/bin:$PATH"
+RUN curl -O https://repo.anaconda.com/archive/Anaconda3-2019.10-Linux-x86_64.sh
+RUN bash Anaconda3-2019.10-Linux-x86_64.sh 
+#RUN pip install numpy torch torchvision pymongo mpi4py
 RUN mkdir /usr/local/cuda/bin
-RUN ln -s /usr/bin/nvcc /usr/local/cuda/bin/nvcc 
-RUN make -C /nccl -j src.build
+RUN ln -s /usr/bin/nvcc /usr/local/cuda/bin/nvcc
+ADD /pyparsa ${LIB}/pyparsa
+RUN make -j -C ${LIB}/pyparsa/nccl src.build
+RUN cd ${LIB}/pyparsa/nccl && python setup.py install
+EXPOSE 29500
+EXPOSE 60000
--- collectives.cc
+++ collectives.cc
@@ -1,15 +1,12 @@
 #include <torch/extension.h>
 #include <nccl.h>
-#include <mpi.h>
 #include <cstdlib>
 #include <iostream>
+#include <unistd.h>
 
 #define CHECK_CUDA(x) TORCH_CHECK(x.type().is_cuda(), #x " must be a CUDA tensor")
 #define CHECK_CONTIGUOUS(x) TORCH_CHECK(x.is_contiguous(), #x " must be contiguous")
 #define CHECK_INPUT(x) CHECK_CUDA(x); CHECK_CONTIGUOUS(x)
-
-#define MPICHECK
-
 
 #define CUDACHECK(cmd) do {                         \
   cudaError_t e = cmd;                              \
@@ -49,44 +46,29 @@ static void getHostName(char* hostname, int maxlen) {
   }
 }
 
-void allreduce(int rank)
+void allreduce(int rank, int nRanks)
 {
   int size = 32*1024*1024;
 
-
-  int myRank, nRanks, localRank = 0;
+  int myRank = rank;
+  int localRank = 0;
     int argc = 1;
      char** argv;
 
-  //initializing MPI
-  MPICHECK(MPI_Init(&argc, &argv));
-  MPICHECK(MPI_Comm_rank(MPI_COMM_WORLD, &myRank));
-  MPICHECK(MPI_Comm_size(MPI_COMM_WORLD, &nRanks));
-
-
-  /*//calculating localRank based on hostname which is used in selecting a GPU
+  //calculating localRank based on hostname which is used in selecting a GPU
   uint64_t hostHashs[nRanks];
   char hostname[1024];
   getHostName(hostname, 1024);
   hostHashs[myRank] = getHostHash(hostname);
-  MPICHECK(MPI_Allgather(MPI_IN_PLACE, 0, MPI_DATATYPE_NULL, hostHashs, sizeof(uint64_t), MPI_BYTE, MPI_COMM_WORLD));
-  for (int p=0; p<nRanks; p++) {
-     if (p == myRank) break;
-     if (hostHashs[p] == hostHashs[myRank]) localRank++;
-  }
-
-
   ncclUniqueId id;
   ncclComm_t comm;
   float *sendbuff, *recvbuff;
   cudaStream_t s;
-
+  std::cout << hostname << std::endl;
 
   //get NCCL unique ID at rank 0 and broadcast it to all others
   if (myRank == 0) ncclGetUniqueId(&id);
-  MPICHECK(MPI_Bcast((void *)&id, sizeof(id), MPI_BYTE, 0, MPI_COMM_WORLD));
-
-
+  std::cout << id << std::endl;
   //picking a GPU based on localRank, allocate device buffers
   CUDACHECK(cudaSetDevice(localRank));
   CUDACHECK(cudaMalloc(&sendbuff, size * sizeof(float)));
@@ -115,10 +97,6 @@ void allreduce(int rank)
   //finalizing NCCL
   ncclCommDestroy(comm);
 
-
-  //finalizing MPI
-  MPICHECK(MPI_Finalize());
-*/
 
   printf("[MPI Rank %d] Success \n", myRank);
 }
