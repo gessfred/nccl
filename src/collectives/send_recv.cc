f5e8db74f290cd5b929a25367c6a1e97df6860b0
Frédéric Gessler <frederic.gessler@epfl.ch>
2020-03-04 19:09:55 +0100

    Fix send/recv signature

--- src/collectives/send_recv.cc
+++ src/collectives/send_recv.cc
@@ -1,19 +1,19 @@
 #include "enqueue.h"
 #include <iostream>
 
-NCCL_API(ncclResult_t, ncclSend, const void* sendbuff, size_t count, ncclDataType_t datatype,
-    int root, ncclComm_t comm, cudaStream_t stream);
+NCCL_API(ncclResult_t, ncclSend, const int dst, const void* sendbuff, size_t count, ncclDataType_t datatype,
+    ncclComm_t comm, cudaStream_t stream);
 ncclResult_t  ncclSend(const int dst, const void* sendbuff, size_t count, ncclDataType_t datatype,
-    int root, ncclComm_t comm, cudaStream_t stream) {
+    ncclComm_t comm, cudaStream_t stream) {
     std::cout << "ncclSend" << std::endl;
     INFO(NCCL_INIT, "ncclSend"); 
     return ncclSuccess;
 }
 
 NCCL_API(ncclResult_t, ncclRecv, const int src, const void* recvbuff, size_t count, ncclDataType_t datatype,
- int root, ncclComm_t comm, cudaStream_t stream)
+ ncclComm_t comm, cudaStream_t stream)
 ncclResult_t  ncclRecv(const int src, const void* recvbuff, size_t count, ncclDataType_t datatype,
- int root, ncclComm_t comm, cudaStream_t stream) {
+ ncclComm_t comm, cudaStream_t stream) {
         std::cout << "ncclRecv" << std::endl;
         INFO(NCCL_INIT, "ncclRecv"); 
         return ncclSuccess;
