e63f6293b41d349b414eed143011fcffcacf4b82
Frédéric Gessler <frederic.gessler@epfl.ch>
2020-03-06 16:46:05 +0100

    Ignore recv error

--- src/collectives/send_recv.cc
+++ src/collectives/send_recv.cc
@@ -27,14 +27,14 @@ ncclResult_t  ncclSend(const int dst, const void* sendbuff, size_t count, ncclDa
     return ncclSuccess;
 }
 
-NCCL_API(ncclResult_t, ncclRecv, const int src, void* recvbuff, size_t count, ncclDataType_t datatype,
+NCCL_API(ncclResult_t, ncclRecv, const int src, const void* recvbuff, size_t count, ncclDataType_t datatype,
  ncclComm_t comm, cudaStream_t stream);
-ncclResult_t  ncclRecv(const int src, void* recvbuff, size_t count, ncclDataType_t datatype,
+ncclResult_t  ncclRecv(const int src, const void* recvbuff, size_t count, ncclDataType_t datatype,
  ncclComm_t comm, cudaStream_t stream) {
     INFO(NCCL_INIT, "ncclRecv"); 
     struct CollectiveArgs args;
     struct ncclInfo info;
-    info.recvbuff = recvbuff;
+    //info.recvbuff = recvbuff;
     info.comm = comm;
     info.stream = stream;
     info.chunkSteps = BROADCAST_CHUNKSTEPS;
